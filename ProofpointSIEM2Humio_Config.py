17b0f1484dae44c649531c57ebbe42b2ec48fbe2
jon <jonathan.headley@cat.com>
2023-09-15 15:17:39 -0500

    Refactored these to remove all classes. Classes aren't needed here, functions will work just fine. Send2HumioHEC.py: I removed all logging and moved that over to the main function so this is a more pure function
    ProofpointSIEM2Humio_Main: Removed the if statements since we can combine those using the event_types and a loop. Also, this is using the requests.auth.HTTPBasicAuth to create the SIEM API authentication method. This now combines all of the events into a newline delimited list of json objects since Humio can handles data pushes as such.
    
    I also add a way to persist the last time the query sent data for. hardcoding the sinceSeconds=3600 was inflexible so this script can now be installed and run via cron or a timer and will pick up contiguous data
    
    ProofpointSIEM2Humio_Config.py: removed the hardcode params to the SIEM API. Also mapped the tokens for each event_type to make it easier to call

--- ProofpointSIEM2Humio_Config.py
+++ ProofpointSIEM2Humio_Config.py
@@ -4,12 +4,13 @@ import logging
 #Set Logging Level and file name
 pfpt_log_level = logging.DEBUG
 log_file = 'PFPT_SIEMapi2Humio.log'
+last_successful_time_File = 'last_success_time'
 
 #Code version - do not alter
 pfpt_version = '1.0'
 
 #####Proofpoint API Config Information
-SIEM_API_url = 'https://tap-api-v2.proofpoint.com/v2/siem/all?format=json&sinceSeconds=3600'
+SIEM_API_url = 'https://tap-api-v2.proofpoint.com/v2/siem/all'
 SIEM_API_user_name = ''
 SIEM_API_password = ''
 
@@ -22,11 +23,17 @@ HumioHECurl = Humio_base+'/api/v1/ingest/hec/raw'
 HumioHECContent_pfpt  = "{'Content-Type': 'application/json', 'Accept':'application/json'}"
 HumioHECverify = False
 
-#Humio HEC Token for Clicks Permitted events
-HumioHECtoken_pfpt_ClicksPermitted = ''
-#Humio HEC Token for Clicks Blocked events
-HumioHECtoken_pfpt_ClicksBlocked = ''
-#Humio HEC Token for Messages Delivered events
-HumioHECtoken_pfpt_MessagesDelivered = ''
-#Humio HEC Token for Messages Blocked events
-HumioHECtoken_pfpt_MessagesBlocked = ''
+# Token Mapping
+HumioHECTokens = {
+    #Humio HEC Token for Clicks Permitted events
+    'clicksPermitted': '',
+
+    #Humio HEC Token for Clicks Blocked events
+    'clicksBlocked': '',
+
+    #Humio HEC Token for Messages Delivered events
+    'messagesDelivered': '',
+    
+    #Humio HEC Token for Messages Blocked events
+    'messagesBlocked': ''
+}
